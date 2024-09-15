4c6ffc01274433144ec3251057ddce6f4a1c0598
PingoMTZ <a222207881@unison.mx>
2024-09-15 12:20:42 -0700

    Se modifico Datos_Chihuahua y se crearon histogramas de edades

--- scripts/var-edad.R
+++ scripts/var-edad.R
@@ -1,12 +1,44 @@
-ggplot(data = Datos_Chihuahua, aes(x = EDAD)) +
-  geom_histogram(
-    fill = "#00CDCD",          # Color de relleno de las barras
-    color = "black",           # Color del borde de las barras
-    binwidth = 5                # Ancho de las barras del histograma (puedes ajustar este valor según tus datos)
-  ) +
-  labs(
-    title = "Distribución de Edades",  # Título del gráfico
-    x = "Edad",                       # Etiqueta del eje x
-    y = "Frecuencia"                  # Etiqueta del eje y
-  ) +
-  theme_minimal() 
+summary(Datos_Chihuahua$EDAD)
+hist(Datos_Chihuahua$EDAD)
+
+# edad por paciente
+ggplot(Datos_Chihuahua, aes(x = EDAD, fill = TIPO_PACIENTE)) +
+  geom_histogram(alpha = 0.6, position = "identity", binwidth = 3) +
+  scale_fill_manual(values = c("blue", "pink")) +
+  labs(title = "Histograma de edades por paciente", x = "Edad", y = "") +
+  theme_minimal()
+
+# edad por sexo
+ggplot(Datos_Chihuahua, aes(x = EDAD, fill = SEXO)) +
+  geom_histogram(alpha = 0.6, position = "identity", binwidth = 3) +
+  scale_fill_manual(values = c("pink", "blue")) +
+  labs(title = "Histograma de edades por sexo", x = "Edad", y = "") +
+  theme_minimal()
+
+# edad por diabetes
+ggplot(Datos_Chihuahua, aes(x = EDAD, fill = DIABETES)) +
+  geom_histogram(alpha = 0.6, position = "identity", binwidth = 3) +
+  scale_fill_manual(values = c("blue", "pink")) +
+  labs(title = "Histograma de edades por paciente con diabetes", x = "Edad", y = "") +
+  theme_minimal()
+
+# edad por obesidad
+ggplot(Datos_Chihuahua, aes(x = EDAD, fill = OBESIDAD)) +
+  geom_histogram(alpha = 0.6, position = "identity", binwidth = 3) +
+  scale_fill_manual(values = c("blue", "pink")) +
+  labs(title = "Histograma de edades por paciente con obesidad", x = "Edad", y = "") +
+  theme_minimal()
+
+# edad por UCI
+ggplot(Datos_Chihuahua, aes(x = EDAD, fill = UCI)) +
+  geom_histogram(alpha = 0.6, position = "identity", binwidth = 3) +
+  scale_fill_manual(values = c("blue", "pink")) +
+  labs(title = "Histograma de edades por UCI", x = "Edad", y = "") +
+  theme_minimal()
+
+# edad por intubado
+ggplot(Datos_Chihuahua, aes(x = EDAD, fill = INTUBADO)) +
+  geom_histogram(alpha = 0.6, position = "identity", binwidth = 3) +
+  scale_fill_manual(values = c("blue", "pink")) +
+  labs(title = "Histograma de edades por paciente intubado", x = "Edad", y = "") +
+  theme_minimal()
